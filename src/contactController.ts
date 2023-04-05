d098d36076f3068bf5f1d744e0720de227008e59
Yoni FIRROLONI <firroloni.yoni@gmail.com>
2023-04-05 17:51:13 +0200

    fynamic routing

--- src/contactController.ts
+++ src/contactController.ts
@@ -13,5 +13,17 @@ contactRouter.get('/', (req, res) => {
     }]);
 })
 
+contactRouter.get('/:id([0-9]+)', (req, res) => {
+    const id = req.params.id
+    res.contentType('application/json');
+    res.send([{
+        id: id,
+        phone: "06 43 45 23 75",
+        email: "example@hotmail.fr",
+        name: "Robichu",
+        firstName: "Alfonce"
+    }]);
+})
+
 export default contactRouter;
 
